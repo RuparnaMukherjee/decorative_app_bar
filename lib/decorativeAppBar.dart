fe51ac6f79376d1d257cba85f7137eff6526b9bd
Ruparna <rupu3097@gmail.com>
2020-09-23 23:41:19 +0530

    -

--- lib/decorativeAppBar.dart
+++ lib/decorativeAppBar.dart
@@ -2,89 +2,46 @@ library decorative_app_bar;
 
 import 'package:flutter/material.dart';
 
-// class DecorativeAppBar extends StatefulWidget {
-//   double barHeight, barPad, radii;
-//   Color gradient1, gradient2, background;
-//   Widget extra;
+class DecorativeAppBar extends StatefulWidget {
+  double barHeight, barPad, radii;
+  Color gradient1, gradient2, background;
+  Widget extra;
 
-//   DecorativeAppBar(
-//       {Key key,
-//       @required this.barHeight,
-//       @required this.barPad,
-//       @required this.radii,
-//       this.background = Colors.white,
-//       this.gradient1 = Colors.white,
-//       this.gradient2 = const Color(0xffffccbc),
-//       this.extra})
-//       : super(key: key);
-//   @override
-//   _DecorativeAppBarState createState() => _DecorativeAppBarState();
-// }
+  DecorativeAppBar(
+      {Key key,
+      @required this.barHeight,
+      @required this.barPad,
+      @required this.radii,
+      this.background = Colors.white,
+      this.gradient1 = Colors.white,
+      this.gradient2 = const Color(0xffffccbc),
+      this.extra})
+      : super(key: key);
+  @override
+  _DecorativeAppBarState createState() => _DecorativeAppBarState();
+}
 
-// class _DecorativeAppBarState extends State<DecorativeAppBar> {
-//   @override
-//   Widget build(BuildContext context) {
-//     double z = widget.barHeight - widget.barPad;
-//     return Container(
-//         height: widget.barHeight,
-//         decoration: BoxDecoration(
-//           gradient:
-//               LinearGradient(colors: [widget.gradient1, widget.gradient2]),
-//         ),
-//         child: Stack(children: [
-//           if (widget.extra != null) widget.extra,
-//           Column(
-//             children: [
-//               Container(
-//                 height: z,
-//                 margin: EdgeInsets.only(top: widget.barPad),
-//                 decoration: BoxDecoration(
-//                   color: widget.background,
-//                   borderRadius:
-//                       BorderRadius.vertical(top: Radius.circular(widget.radii)),
-//                   boxShadow: [
-//                     BoxShadow(
-//                       color: Colors.grey.withOpacity(0.2),
-//                       blurRadius: 5,
-//                       spreadRadius: 0.2,
-//                       offset: Offset(0.5, -8), // changes position of shadow
-//                     ),
-//                   ],
-//                 ),
-//               )
-//             ],
-//           ),
-//         ]));
-//   }
-// }
-
-class DecorativeAppBar {
-  // ignore: non_constant_identifier_names
-  AppBar(
-      {@required double barHeight,
-      @required double barPad,
-      @required double radii,
-      Color background = Colors.white,
-      Color gradient1 = Colors.white,
-      Color gradient2 = const Color(0xffffccbc),
-      Widget extra}) async {
-    double z = barHeight - barPad;
+class _DecorativeAppBarState extends State<DecorativeAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    double z = widget.barHeight - widget.barPad;
     return Container(
-        height: barHeight,
+        height: widget.barHeight,
         decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [gradient1, gradient2]),
+          gradient:
+              LinearGradient(colors: [widget.gradient1, widget.gradient2]),
         ),
         child: Stack(children: [
-          if (extra != null) extra,
+          if (widget.extra != null) widget.extra,
           Column(
             children: [
               Container(
                 height: z,
-                margin: EdgeInsets.only(top: barPad),
+                margin: EdgeInsets.only(top: widget.barPad),
                 decoration: BoxDecoration(
-                  color: background,
+                  color: widget.background,
                   borderRadius:
-                      BorderRadius.vertical(top: Radius.circular(radii)),
+                      BorderRadius.vertical(top: Radius.circular(widget.radii)),
                   boxShadow: [
                     BoxShadow(
                       color: Colors.grey.withOpacity(0.2),
@@ -100,3 +57,46 @@ class DecorativeAppBar {
         ]));
   }
 }
+
+// class DecorativeAppBar {
+//   // ignore: non_constant_identifier_names
+//   AppBar(
+//       {@required double barHeight,
+//       @required double barPad,
+//       @required double radii,
+//       Color background = Colors.white,
+//       Color gradient1 = Colors.white,
+//       Color gradient2 = const Color(0xffffccbc),
+//       Widget extra}) async {
+//     double z = barHeight - barPad;
+//     return Container(
+//         height: barHeight,
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(colors: [gradient1, gradient2]),
+//         ),
+//         child: Stack(children: [
+//           if (extra != null) extra,
+//           Column(
+//             children: [
+//               Container(
+//                 height: z,
+//                 margin: EdgeInsets.only(top: barPad),
+//                 decoration: BoxDecoration(
+//                   color: background,
+//                   borderRadius:
+//                       BorderRadius.vertical(top: Radius.circular(radii)),
+//                   boxShadow: [
+//                     BoxShadow(
+//                       color: Colors.grey.withOpacity(0.2),
+//                       blurRadius: 5,
+//                       spreadRadius: 0.2,
+//                       offset: Offset(0.5, -8), // changes position of shadow
+//                     ),
+//                   ],
+//                 ),
+//               )
+//             ],
+//           ),
+//         ]));
+//   }
+// }
